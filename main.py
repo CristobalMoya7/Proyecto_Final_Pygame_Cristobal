ee6b7a858be39441b66ddb8d2460e4774030d6c6
CristobalMoya7 <cristobalmoyalorente00@gmail.com>
2023-01-31 21:53:12 +0100

    Comienzo proyecto, creacion pantalla

---
+++ main.py
@@ -0,0 +1,1 @@
+from thequest.game import Quest
---
+++ thequest/game.py
@@ -0,0 +1,13 @@
+import pygame as pg
+import os
+from . import ALTO_P, ANCHO_P
+
+class Quest:
+    def __init__(self):
+        pg.init()
+        self.display = pg.display.set_mode((ANCHO_P, ALTO_P))
+        icono_juego = pg.image.load(os.path.join(
+            "resources", "images", "icono_planeta.png"))
+        pg.display.set_icon(icono_juego)
+        pg.display.set_caption("THE QUEST")
+        pg.mixer.init()
